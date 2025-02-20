a49bbd4f8d5d8bd06ff1353f9775e0d7a60db0a1
alexandergreif <133988732+alexandergreif@users.noreply.github.com>
2025-02-20 14:22:39 +0100

    added the import for dotenv, outsourced the api key to the .env file which is not listed in the git repo cause of security risks. Added a part to the file where the API_KEY gets loaded from the .env file.

--- data_fetcher.py
+++ data_fetcher.py
@@ -1,9 +1,13 @@
 import requests
-import json
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+API_KEY = os.getenv('API_KEY')
+
 
 def fetch_data(name):
     """Fetch animal data from the API for the given name."""
-    API_KEY = 'YiLRKz33s/9rKRZAVD1gag==2Ue5RVRY4ijqhCOl'
     API_URL = f'https://api.api-ninjas.com/v1/animals?name={name}'
     headers = {'X-Api-Key': API_KEY}
     response = requests.get(API_URL, headers=headers)
